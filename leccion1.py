e2cbcf4e3b8d1a495639408307e99f47abe37748
Smith Aceituno Trucios <smith.a.trucios@gmail.com>
2026-02-13 18:24:28 -0500

    Actualizando mi guia de comandos con el truco del punto

---
+++ leccion1.py
@@ -0,0 +1,9 @@
+# 1ra prueba de código en Python
+nombre = "Smith"
+programa = "Pyhton"
+print(f"Hola, soy {nombre} y estoy aprendiendo a usar {programa} en 2026.")
+print(f"El día de hoy aprenderemos a usar {programa} para crear una calculadora.")
+a = 10
+b = 8
+suma = a + b
+print(f"Es importante saber que la suma de {a} y {b} es: {suma}")
