2dcc1cbaef0d6480404dbd22af703085242d090d
Smith Aceituno Trucios <smith.a.trucios@gmail.com>
2026-02-14 07:15:08 -0500

    Actualizando mi guia de comandos con el truco del punto

--- leccion1.py
+++ leccion1.py
@@ -1,9 +1,16 @@
 # 1ra prueba de código en Python
 nombre = "Smith"
-programa = "Pyhton"
+programa = "Python"
 print(f"Hola, soy {nombre} y estoy aprendiendo a usar {programa} en 2026.")
 print(f"El día de hoy aprenderemos a usar {programa} para crear una calculadora.")
 a = 10
 b = 8
 suma = a + b
+resta = a - b
+multiplicación = a * b 
+división = a / b 
 print(f"Es importante saber que la suma de {a} y {b} es: {suma}")
+print(f"La resta de {a} y {b} es: {resta}")
+print(f"La multiplicación de {a} y {b} es:{multiplicación}")
+print(f"La división de {a} y {b} es: {división}")
+
